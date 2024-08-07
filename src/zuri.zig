b6c42f672b0410f66f5115880a6f59738925295b
pchchv <ipchchv@gmail.com>
2024-08-07 08:39:34 +0300

    Implement a mapQuery method

--- src/zuri.zig
+++ src/zuri.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 const net = std.net;
+const Allocator = std.mem.Allocator;
+
+const ValueMap = std.StringHashMap([]const u8);
 
 /// Host - possible uri host values
 pub const Host = union(enum) {
@@ -23,4 +26,38 @@ pub const Uri = struct {
     username: []const u8,
     password: []const u8,
     fragment: []const u8,
+
+    /// mapQuery maps query strings into a hashmap of key-value pairs, with no value being an empty string.
+    pub fn mapQuery(allocator: Allocator, query: []const u8) Allocator.Error!ValueMap {
+        if (query.len == 0) {
+            return ValueMap.init(allocator);
+        }
+
+        var map = ValueMap.init(allocator);
+        errdefer map.deinit();
+
+        var start: usize = 0;
+        var mid: usize = 0;
+        for (query, 0..) |c, i| {
+            if (c == '&') {
+                if (mid != 0) {
+                    _ = try map.put(query[start..mid], query[mid + 1 .. i]);
+                } else {
+                    _ = try map.put(query[start..i], "");
+                }
+                start = i + 1;
+                mid = 0;
+            } else if (c == '=') {
+                mid = i;
+            }
+        }
+
+        if (mid != 0) {
+            _ = try map.put(query[start..mid], query[mid + 1 ..]);
+        } else {
+            _ = try map.put(query[start..], "");
+        }
+
+        return map;
+    }
 };
