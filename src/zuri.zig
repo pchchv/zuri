7ecda4f058b97cc9155061aaa92809328027b363
pchchv <ipchchv@gmail.com>
2024-08-07 08:32:32 +0300

    Implement a Uri

--- src/zuri.zig
+++ src/zuri.zig
@@ -6,3 +6,15 @@ pub const Host = union(enum) {
     ip: net.Address,
     name: []const u8,
 };
+
+pub const Uri = struct {
+    len: usize,
+    host: Host,
+    port: ?u16,
+    path: []const u8,
+    query: []const u8,
+    scheme: []const u8,
+    username: []const u8,
+    password: []const u8,
+    fragment: []const u8,
+};
