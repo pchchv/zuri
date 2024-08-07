23eda528621260325d43030681309c979b69bba7
pchchv <ipchchv@gmail.com>
2024-08-07 08:35:05 +0300

    Fix

--- src/zuri.zig
+++ src/zuri.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const net = std.net;
 
-/// possible uri host values
+/// Host - possible uri host values
 pub const Host = union(enum) {
     ip: net.Address,
     name: []const u8,
